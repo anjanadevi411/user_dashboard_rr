a9025761991ecff2f02bd7c33bc713fb6b90fa27
Anjana Devi <anjanadevi411@gmail.com>
2022-03-10 13:32:02 +0100

    changes in store

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,10 +4,6 @@ import thunk from "redux-thunk";
 import rootReducer from "./reducers/index";
 
 const store = () => {
-  //getting initial state from cart
-  const userArray = localStorage.getItem("users");
-  const users = userArray ? JSON.parse(userArray) : [];
-
   const middlewares = [thunk];
   const preloadedState = {
     reducerUsers: {
@@ -15,38 +11,12 @@ const store = () => {
       user: {},
     },
   };
-
   const reduxStore = createStore(
     rootReducer,
     preloadedState,
     composeWithDevTools(applyMiddleware(...middlewares))
   );
 
-  reduxStore.subscribe(() => {
-    const currentState = reduxStore.getState();
-
-    const usersState = currentState.reducerUsers.users;
-    const stringifyUser = JSON.stringify(usersState);
-    localStorage.setItem("user", stringifyUser);
-  });
   return reduxStore;
 };
-
 export default store;
-
-//   const middlewares = [thunk];
-//   const preloadedState = {
-//     reducerUsers: {
-//       users: [],
-//       user: {},
-//     },
-//   };
-//   const reduxStore = createStore(
-//     rootReducer,
-//     preloadedState,
-//     composeWithDevTools(applyMiddleware(...middlewares))
-//   );
-
-//   return reduxStore;
-// };
-// export default store;
